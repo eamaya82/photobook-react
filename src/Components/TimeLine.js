6472821c75b96a2bd6a8407d1b229b851cf6ae51
Edgardo Amaya <eamaya82@gmail.com>
2018-07-28 11:58:31 -0500

    timeline css

--- src/Components/TimeLine.js
+++ src/Components/TimeLine.js
@@ -1,29 +1,19 @@
 import React,{Component} from 'react'
+import './../css/timeline.css'
 
 class Timeline extends Component{
   render(){
     return (
-      <section>
-      <article>
-        <h2>lorem ipsum</h2>
-        <figure>
-          <img src="https://dummyimage.com/400x400/bfbfbf/fff" alt="imagen"/>
-        </figure>
-        <section>
-          <p>10 Me gusta</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit gravida rhoncus, mi porttitor dictum nascetur nullam venenatis sociosqu orci, leo tincidunt aliquet condimentum platea litora torquent facilisi. </p>
-          <p>Dec 15, 2018</p>
-        </section>
-      </article>
+      <section class="timeline">
         <article>
           <h2>lorem ipsum</h2>
           <figure>
             <img src="https://dummyimage.com/400x400/bfbfbf/fff" alt="imagen"/>
           </figure>
           <section>
-            <p>10 Me gusta</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit gravida rhoncus, mi porttitor dictum nascetur nullam venenatis sociosqu orci, leo tincidunt aliquet condimentum platea litora torquent facilisi. </p>
-            <p>Dec 15, 2018</p>
+            <p class="like">10 Me gusta</p>
+            <p class="desc">Lorem ipsum dolor sit amet consectetur adipiscing elit gravida rhoncus, mi porttitor dictum nascetur nullam venenatis sociosqu orci, leo tincidunt aliquet condimentum platea litora torquent facilisi. </p>
+            <p class="date">Dec 15, 2018</p>
           </section>
         </article>
         <article>
@@ -32,9 +22,20 @@ class Timeline extends Component{
             <img src="https://dummyimage.com/400x400/bfbfbf/fff" alt="imagen"/>
           </figure>
           <section>
-            <p>10 Me gusta</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit gravida rhoncus, mi porttitor dictum nascetur nullam venenatis sociosqu orci, leo tincidunt aliquet condimentum platea litora torquent facilisi. </p>
-            <p>Dec 15, 2018</p>
+            <p class="like">10 Me gusta</p>
+            <p class="desc">Lorem ipsum dolor sit amet consectetur adipiscing elit gravida rhoncus, mi porttitor dictum nascetur nullam venenatis sociosqu orci, leo tincidunt aliquet condimentum platea litora torquent facilisi. </p>
+            <p class="date">Dec 15, 2018</p>
+          </section>
+        </article>
+        <article>
+          <h2>lorem ipsum</h2>
+          <figure>
+            <img src="https://dummyimage.com/400x400/bfbfbf/fff" alt="imagen"/>
+          </figure>
+          <section>
+            <p class="like">10 Me gusta</p>
+            <p class="desc">Lorem ipsum dolor sit amet consectetur adipiscing elit gravida rhoncus, mi porttitor dictum nascetur nullam venenatis sociosqu orci, leo tincidunt aliquet condimentum platea litora torquent facilisi. </p>
+            <p class="date">Dec 15, 2018</p>
           </section>
         </article>
       </section>
